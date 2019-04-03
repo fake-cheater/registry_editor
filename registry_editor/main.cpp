c2c24411e409278f2fcfb65eaad687638bc930ef
paracorded <yusuif@riseup.net>
2019-04-03 08:13:14 +0200

    changes

--- registry_editor/main.cpp
+++ registry_editor/main.cpp
@@ -5,8 +5,6 @@
 #include <array>
 #include "raii.hpp"
 
-// this is one of the worst applications I have ever written in my life, atleast it's modern.
-
 #define out( text, ... ) std::printf( text, ##__VA_ARGS__ )
 
 #pragma warning( disable : 4312 )
@@ -26,7 +24,7 @@ int main( ) {
 	static const auto start_time = std::chrono::system_clock::now( ).time_since_epoch( );
 
 	HKEY output = nullptr;
-	auto open_status = RegOpenKeyExA( HKEY_LOCAL_MACHINE, "System\\CurrentControlSet\\Control", NULL, KEY_ALL_ACCESS, &output );
+	auto open_status = RegOpenKeyExA( HKEY_LOCAL_MACHINE, "System\\CurrentControlSet\\Control", 0, KEY_ALL_ACCESS, &output );
 
 	if ( open_status != ERROR_SUCCESS ) {
 		out( "[-] failed to open handle to System\\CurrentControlSet\\Control\n" );
@@ -35,17 +33,18 @@ int main( ) {
 		return EXIT_FAILURE;
 	}
 
-	raii::hkey control_key( output ); {
+	raii::hkey control_key( output ); 
+	{
 		std::string randomized_string;
 		randomized_string.resize( string_length );
 		std::generate_n( randomized_string.begin( ), string_length, random_char );
 
-		std::array< const char*, 2 > sub_keys = { "SystemInformation", "ComputerHardwareId" };
+		std::array sub_keys{ "SystemInformation", "ComputerHardwareId" };
 
 		auto set_status = ERROR_SUCCESS;
 
 		for ( auto idx = 0; idx < sub_keys.size( ); idx++ ) {
-			set_status = RegSetValueExA( control_key.get( ), sub_keys[ idx ], NULL, REG_SZ, ( std::uint8_t* )randomized_string.c_str( ), string_length );
+			set_status = RegSetValueExA( control_key.get( ), sub_keys[ idx ], 0, REG_SZ, ( std::uint8_t* )randomized_string.c_str( ), string_length );
 
 			if ( set_status == ERROR_SUCCESS )
 				out( "[+] set %s to: %s\n", sub_keys[ idx ], randomized_string.c_str( ) );
@@ -55,7 +54,7 @@ int main( ) {
 	}
 
 	output = nullptr;
-	open_status = RegOpenKeyExA( HKEY_LOCAL_MACHINE, "Hardware\\Description\\System\\BIOS", NULL, KEY_ALL_ACCESS, &output );
+	open_status = RegOpenKeyExA( HKEY_LOCAL_MACHINE, "Hardware\\Description\\System\\BIOS", 0, KEY_ALL_ACCESS, &output );
 
 	if ( open_status != ERROR_SUCCESS ) {
 		out( "[-] failed to open handle to Hardware\\Description\\System\\BIOS\n" );
@@ -64,17 +63,18 @@ int main( ) {
 		return EXIT_FAILURE;
 	}
 
-	raii::hkey bios_key( output ); {
+	raii::hkey bios_key( output ); 
+	{
 		std::string randomized_string;
 		randomized_string.resize( string_length );
 		std::generate_n( randomized_string.begin( ), string_length, random_char );
 
-		std::array< const char*, 4 > sub_keys = { "BIOSVendor", "BIOSReleaseDate", "SystemManufacturer", "SystemProductName" };
+		std::array sub_keys{ "BIOSVendor", "BIOSReleaseDate", "SystemManufacturer", "SystemProductName" };
 
 		auto set_status = ERROR_SUCCESS;
 
 		for ( auto idx = 0; idx < sub_keys.size( ); idx++ ) {
-			set_status = RegSetValueExA( bios_key.get( ), sub_keys[ idx ], NULL, REG_SZ, ( std::uint8_t* )randomized_string.c_str( ), string_length );
+			set_status = RegSetValueExA( bios_key.get( ), sub_keys[ idx ], 0, REG_SZ, ( std::uint8_t* )randomized_string.c_str( ), string_length );
 
 			if ( set_status == ERROR_SUCCESS )
 				out( "[+] set %s to: %s\n", sub_keys[ idx ], randomized_string.c_str( ) );
@@ -84,7 +84,7 @@ int main( ) {
 	}
 
 	output = nullptr;
-	open_status = RegOpenKeyExA( HKEY_LOCAL_MACHINE, "Hardware\\DeviceMap\\Scsi\\Scsi Port 0\\Scsi Bus 0\\Target Id 0\\Logical Unit Id 0", NULL, KEY_ALL_ACCESS, &output );
+	open_status = RegOpenKeyExA( HKEY_LOCAL_MACHINE, "Hardware\\DeviceMap\\Scsi\\Scsi Port 0\\Scsi Bus 0\\Target Id 0\\Logical Unit Id 0", 0, KEY_ALL_ACCESS, &output );
 
 	if ( open_status != ERROR_SUCCESS ) {
 		out( "[-] failed to open handle to Hardware\\DeviceMap\\Scsi\\Scsi Port 0\\Scsi Bus 0\\Target Id 0\\Logical Unit Id 0\n" );
@@ -93,17 +93,18 @@ int main( ) {
 		return EXIT_FAILURE;
 	}
 
-	raii::hkey scsi_key( output ); {
+	raii::hkey scsi_key( output ); 
+	{
 		std::string randomized_string;
 		randomized_string.resize( string_length );
 		std::generate_n( randomized_string.begin( ), string_length, random_char );
 
-		std::array< const char*, 2 > sub_keys = { "Identifier", "SerialNumber" };
+		std::array sub_keys{ "Identifier", "SerialNumber" };
 
 		auto set_status = ERROR_SUCCESS;
 
 		for ( auto idx = 0; idx < sub_keys.size( ); idx++ ) {
-			set_status = RegSetValueExA( scsi_key.get( ), sub_keys[ idx ], NULL, REG_SZ, ( std::uint8_t* )randomized_string.c_str( ), string_length );
+			set_status = RegSetValueExA( scsi_key.get( ), sub_keys[ idx ], 0, REG_SZ, ( std::uint8_t* )randomized_string.c_str( ), string_length );
 
 			if ( set_status == ERROR_SUCCESS )
 				out( "[+] set %s to: %s\n", sub_keys[ idx ], randomized_string.c_str( ) );
@@ -113,7 +114,7 @@ int main( ) {
 	}
 
 	output = nullptr;
-	open_status = RegOpenKeyExA( HKEY_LOCAL_MACHINE, "Hardware\\Description\\System\\CentralProcessor\\0", NULL, KEY_ALL_ACCESS, &output );
+	open_status = RegOpenKeyExA( HKEY_LOCAL_MACHINE, "Hardware\\Description\\System\\CentralProcessor\\0", 0, KEY_ALL_ACCESS, &output );
 
 	if ( open_status != ERROR_SUCCESS ) {
 		out( "[-] failed to open handle to Hardware\\Description\\System\\CentralProcessor\\0\n" );
@@ -122,12 +123,13 @@ int main( ) {
 		return EXIT_FAILURE;
 	}
 
-	raii::hkey cpu_key( output ); {
+	raii::hkey cpu_key( output ); 
+	{
 		std::string randomized_string;
 		randomized_string.resize( string_length );
 		std::generate_n( randomized_string.begin( ), string_length, random_char );
 
-		auto set_status = RegSetValueExA( cpu_key.get( ), "ProcessorNameString", NULL, REG_SZ, ( std::uint8_t* )randomized_string.c_str( ), string_length );
+		auto set_status = RegSetValueExA( cpu_key.get( ), "ProcessorNameString", 0, REG_SZ, ( std::uint8_t* )randomized_string.c_str( ), string_length );
 
 		if ( set_status == ERROR_SUCCESS )
 			out( "[+] set ProcessorNameString to: %s\n", randomized_string.c_str( ) );
@@ -136,7 +138,7 @@ int main( ) {
 	}
 
 	output = nullptr;
-	open_status = RegOpenKeyExA( HKEY_LOCAL_MACHINE, "System\\CurrentControlSet\\Control\\Class\\{4d36e968-e325-11ce-bfc1-08002be10318}\\0000", NULL, KEY_ALL_ACCESS, &output );
+	open_status = RegOpenKeyExA( HKEY_LOCAL_MACHINE, "System\\CurrentControlSet\\Control\\Class\\{4d36e968-e325-11ce-bfc1-08002be10318}\\0000", 0, KEY_ALL_ACCESS, &output );
 
 	if ( open_status != ERROR_SUCCESS ) {
 		out( "[-] failed to open handle to System\\CurrentControlSet\\Control\\Class\\{4d36e968-e325-11ce-bfc1-08002be10318}\\0000\n" );
@@ -145,23 +147,24 @@ int main( ) {
 		return EXIT_FAILURE;
 	}
 
-	raii::hkey desc_key( output ); {
+	raii::hkey desc_key( output ); 
+	{
 		std::random_device random_device;
 		std::mt19937 mersenne_generator( random_device( ) );
 		std::uniform_int_distribution<> distribution( 0, MAXUINT32 );
 
 		DWORD randomized_nr = static_cast< DWORD >( distribution( mersenne_generator ) );
 
-		auto set_status = RegSetValueExA( desc_key.get( ), "DriverDesc", NULL, REG_DWORD, ( std::uint8_t* )&randomized_nr, sizeof( DWORD ) );
+		auto set_status = RegSetValueExA( desc_key.get( ), "DriverDesc", 0, REG_DWORD, ( std::uint8_t* )&randomized_nr, sizeof( DWORD ) );
 
 		if ( set_status == ERROR_SUCCESS )
-			out( "[+] set DriverDesc to: %i\n", desc_random );
+			out( "[+] set DriverDesc to: %i\n", randomized_nr );
 		else
 			out( "[-] failed to set DriverDesc\n" );
 	}
 
 	output = nullptr;
-	open_status = RegOpenKeyExA( HKEY_LOCAL_MACHINE, "Software\\Microsoft\\Windows NT\\CurrentVersion", NULL, KEY_ALL_ACCESS, &output );
+	open_status = RegOpenKeyExA( HKEY_LOCAL_MACHINE, "Software\\Microsoft\\Windows NT\\CurrentVersion", 0, KEY_ALL_ACCESS, &output );
 
 	if ( open_status != ERROR_SUCCESS ) {
 		out( "[-] failed to open handle to Software\\Microsoft\\Windows NT\\CurrentVersion\n" );
@@ -170,19 +173,20 @@ int main( ) {
 		return EXIT_FAILURE;
 	}
 
-	raii::hkey nt_key( output ); {
+	raii::hkey nt_key( output ); 
+	{
 		std::random_device random_device;
 		std::mt19937 mersenne_generator( random_device( ) );
 		std::uniform_int_distribution<> distribution( 0, MAXUINT32 );
 
 		DWORD randomized_nr = static_cast< DWORD >( distribution( mersenne_generator ) );
-		
-		std::array< const char*, 4 > sub_keys = { "InstallDate", "InstallTime", "BuildGUID", "ProductID" };
+
+		std::array sub_keys{ "InstallDate", "InstallTime", "BuildGUID", "ProductID" };
 
 		auto set_status = ERROR_SUCCESS;
 
 		for ( auto idx = 0; idx < sub_keys.size( ); idx++ ) {
-			set_status = RegSetValueExA( nt_key.get( ), sub_keys[ idx ], NULL, REG_DWORD, ( std::uint8_t* )&randomized_nr, sizeof( DWORD ) );
+			set_status = RegSetValueExA( nt_key.get( ), sub_keys[ idx ], 0, REG_DWORD, ( std::uint8_t* )&randomized_nr, sizeof( DWORD ) );
 
 			if ( set_status == ERROR_SUCCESS )
 				out( "[+] set %s to: %i\n", sub_keys[idx], randomized_nr );
