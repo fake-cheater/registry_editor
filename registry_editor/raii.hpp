4811070bf95cfe28e0f152ccdb4b84493ca3285a
paracorded <yusuif@riseup.net>
2019-04-09 00:26:39 +0200

    Update raii.hpp

--- registry_editor/raii.hpp
+++ registry_editor/raii.hpp
@@ -11,11 +11,11 @@ namespace raii {
 	};
 	
 	struct handle_deleter {
-		const void operator( )( void * key ) noexcept {
+		const void operator( )( const HANDLE key ) noexcept {
 			CloseHandle( key );
 		}
 	};
 
 	using hkey = std::unique_ptr<std::remove_pointer_t<HKEY>, registry_deleter>;
-	using handle = std::unique_ptr<void, handle_deleter>;
+	using handle = std::unique_ptr<std::remove_pointer_t<HANDLE>, handle_deleter>;
 }
